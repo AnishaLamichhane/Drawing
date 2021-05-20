ac387ec6733f184bc3efae517fab515b840a3ce7
Anisha Lamichhane <ansalamichhane111@gmail.com>
2021-05-20 11:26:20 +0545

    Initial Commit

---
+++ project 9/project_9App.swift
@@ -0,0 +1,17 @@
+//
+//  project_9App.swift
+//  project 9
+//
+//  Created by Anisha Lamichhane on 5/20/21.
+//
+
+import SwiftUI
+
+@main
+struct project_9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
