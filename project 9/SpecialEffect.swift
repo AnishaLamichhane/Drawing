9186e33e14c685103fc5ff07b13719e638bfd942
Anisha Lamichhane <ansalamichhane111@gmail.com>
2021-05-24 18:35:02 +0545

    special effects in swift

---
+++ project 9/SpecialEffect.swift
@@ -0,0 +1,50 @@
+//
+//  SpecialEffect.swift
+//  project 9
+//
+//  Created by Anisha Lamichhane on 5/24/21.
+//
+
+import SwiftUI
+
+struct SpecialEffect: View {
+   
+    @State private var amount: CGFloat = 0.0
+
+    var body: some View {
+        VStack {
+            ZStack {
+                Circle()
+                    .fill(Color.red)
+                    .frame(width: 200 * amount)
+                    .offset(x: -50, y: -80)
+                    .blendMode(.screen)
+
+                Circle()
+                    .fill(Color.green)
+                    .frame(width: 200 * amount)
+                    .offset(x: 50, y: -80)
+                    .blendMode(.screen)
+
+                Circle()
+                    .fill(Color.blue)
+                    .frame(width: 200 * amount)
+                    .blendMode(.screen)
+            }
+            .frame(width: 300, height: 300)
+
+            Slider(value: $amount)
+                .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+        .edgesIgnoringSafeArea(.all)
+    }
+    
+}
+
+struct SpecialEffect_Previews: PreviewProvider {
+    static var previews: some View {
+        SpecialEffect()
+    }
+}
